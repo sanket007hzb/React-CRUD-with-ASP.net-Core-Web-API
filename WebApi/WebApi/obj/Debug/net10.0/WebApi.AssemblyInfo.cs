44d2948a7bb7a0aeca67fef00d1415efa4d6f23c
Sanket Jaruhar <sanket007hzb@gmail.com>
2026-05-05 14:34:23 +0530

    Included react files from parent

--- WebApi/WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
+++ WebApi/WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebecb8ddd6ca0d8cac4cc14562d3c5e404ca2ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
