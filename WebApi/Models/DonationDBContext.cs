ebecb8ddd6ca0d8cac4cc14562d3c5e404ca2ecd
Sanket Jaruhar <sanket007hzb@gmail.com>
2026-05-05 13:00:31 +0530

    initial commit

---
+++ WebApi/Models/DonationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options) { }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DonationDBContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
