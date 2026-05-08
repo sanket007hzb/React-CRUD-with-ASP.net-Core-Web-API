3d343fc0239b545359ac6db8c6d7aac2446c3b8e
sanket007hzb <sanket007hzb@gmail.com>
2026-05-08 12:12:05 +0530

    checkpoint before checking out feature/cors-implemented

--- WebApi/WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
+++ WebApi/WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d2948a7bb7a0aeca67fef00d1415efa4d6f23c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3196126ad27394142982bbfd6bf86d43666967ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
